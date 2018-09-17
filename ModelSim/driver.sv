8df601c1228398fbcfe8e1d4a5d3722c250d94d1
Eric Christianson <ewchristians@wisc.edu>
2018-09-16 22:30:09 -0500

    pushing just to pull new changes

--- ModelSim/driver.sv
+++ ModelSim/driver.sv
@@ -34,82 +34,43 @@ module driver(
 // instatntiate SPART
 spart_DUT iDUT(.clk(clk), .rst(rst), .iocs(iocs), .iorw(iorw), .rda(rda), .tbr(tbr), .ioaddr(ioaddr), .databus(databus), .txd(txd), .rxd(rxd));
 
+// BAUD RATE CALCULATONS
+// 4800 = 1042 clocks
+// 9600 = 521 clocks
+// 19200 = 260 clocks
+// 38400 = 130 clocks
+
+
 // TODO: load starting divison buffer values on reset
 always_ff @(posedge clk, negedge rst) begin
 	if (!rst) begin
 		case(br_cfg)
 			2'b00: 
 				begin
-				
+					databus <= 16'h0412;
 				end
 			2'b01:
 				begin
-								// TODO: assign db_buffer to different values depending on the br_cfg
+					databus <= 16'h0209;
 				end
 			2'b10:
 				begin
-				
+					databus <= 16'h0104;
 				end
 			2'b11:
 				begin
-				
+					databus <= 16'h0082;
 				end
 		endcase
 	
 	end
-
+	else
+		databus = db_buffer[15:8];
 
 end
 
-always @(negedge rst) begin
-	iocs = 1'b0;
-	iorw = 1'b0;
-	ioaddr = 2'b10;  //division buffer low to start
-	databus = 8'b0;
+always @(*) begin
 	
-	repeat(2) @(negedge clk);
-	
-	databus = db_buffer[7:0];
-	
-	repeat(2) @(negedge clk);
-	
-	iocs = 1'b1;
-	
-	repeat(2) @(negedge clk);
-	
-	iocs = 1'b0;
-	ioaddr = 2'b11;
-	databus = db_buffer[15:8]
-	
-	repeat(2) @(negedge clk);
-
-	iocs = 1'b1;
-
-	repeat(2) @(negedge clk);
-	
-	iocs = 1'b0;
-	ioaddr = 2'b00;
-	databus = 8'b01101101;
-	
-	repeat(2) @(negedge clk);
-	
-	iocs = 1'b1;
-	while(!tbr);
-	
-	repeat(15) @(negedge clk);
-	
-	iocs = 1'b0;
-	iorw = 1'b1;
-	databus = 8'bzzzzzzzz;
-	
-	repeat(2) @(negedge clk);
-	
-	iocs = 1'b1;
-	while(!rda);
-
-	repeat(2) @(negedge clk);
-	
-	$stop;
 
 
 
