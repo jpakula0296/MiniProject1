3decfa0ae75a7ebfaa5a24dd897dbfeee7886dbc
Jesse Pakula <jpakula@X-ES.COM>
2018-09-17 12:01:05 -0500

    removed ioaddr case statement and control "enable" signals for writes. all registers that can be written to now latch when the approprate signals are set (iocs, iorw, tbr for transmit buffer, ioaddr for each reg). the write state is no longer needed

--- ModelSim/spart.sv
+++ ModelSim/spart.sv
@@ -41,16 +41,11 @@ reg [7:0] division_buffer_high;
 reg [7:0] receive_buffer;
 reg [7:0] status;
 reg [7:0] read_data; // multiplexed output of receive buffer and status reg for databus read (ioaddr)
-reg [7:0] write_data; // multiplexed output of transmit buffer, DB(Low), and DB(High) based on ioaddr
 					 
 logic clr;
-logic transmit_buffer_en = iocs & !iorw; // enables loading databus into transmission buffer
 logic receive_buffer_en;  // enables receiving of data to begin from IO device
 logic status_register_read;
 logic status_read; // high when reading status register for rda and tbr
-
-logic db_high_en;
-logic db_low_en;
 
 // Instantiate rx and tx modules
 spart_rx rx_mod(
@@ -78,6 +73,9 @@ state_t state, next_state;
 // otherwise high z since SPART will be reading it
 assign databus = (iorw) ? read_data : 8'bz;
 
+assign divisor_buffer = {division_buffer_high[7:0], division_buffer_low[7:0]}; // concatenate for buffer
+
+
 // first bit of ioaddr is select signal for multiplexer, choosing between status reg and receive_buffer
 assign read_data = (ioaddr[0]) ? status : receive_buffer;
 
@@ -87,9 +85,8 @@ assign status = {6'b000000, tbr, rda};
 // DB_HIGH flop, only load it from data bus when enable signal goes high for 1 clk cycle
 always_ff @(posedge clk, negedge rst) begin
 	if(!rst)
-		// TODO division buffer needs to load starting value based on DIP switches
 		division_buffer_high <= 8'b0;
-	else if(db_high_en)
+	else if((ioaddr == 2'b11) && iocs && !iorw)
 		division_buffer_high <= databus;
 	else
 		division_buffer_high <= division_buffer_high;
@@ -98,21 +95,19 @@ end
 // DB_LOW flop, only load on enable, otherwise hold value
 always_ff @(posedge clk, negedge rst) begin
 	if(!rst)
-		// TODO load correct starting value based on switches.
 		division_buffer_low <= 8'b0;
-	else if(db_low_en)
+	else if((ioaddr == 2'b10) && iocs && !iorw)
 		division_buffer_low <= databus;
 	else
 		division_buffer_low <= division_buffer_low;
 end
 
-assign divisor_buffer = {division_buffer_high[7:0], division_buffer_low[7:0]}; // concatenate for buffer
 
 // receive_buffer latches rx_shift data bits when rx_tx_buf_full is asserted
 always_ff @(posedge clk, negedge rst) begin
 	if (!rst)
 		receive_buffer <= 8'b0;
-	else if (rda) // THIS MAY NEED TO BE DELAYED ONE CLOCK CYCLE
+	else if (rda)
 		receive_buffer <= rx_shift_reg[8:1]; // data bits in middle, start/stop bits on end
 	else
 		receive_buffer <= receive_buffer; // intentional latch
@@ -122,46 +117,18 @@ end
 always_ff @(posedge clk, negedge rst) begin
 	if (!rst)
 		transmit_buffer <= 8'b0;
-	else if (transmit_buffer_en)
+	else if ((ioaddr == 2'b00) && iocs && !iorw && tbr)
 		transmit_buffer <= databus;  
 	else
 		transmit_buffer <= transmit_buffer; // intentional latch
 end
 
 
-always @(posedge clk, negedge rst) begin
-	receive_buffer_en = 1'b0;
-	db_high_en = 1'b0;
-	db_low_en = 1'b0;
-	status_read = 1'b0;
-	case(ioaddr)
-		2'b00:  
-			begin
-				if(iorw)
-					receive_buffer_en <= 1'b1;
-			end
-		2'b01:  
-			begin
-				if(iorw)
-					status_read = 1'b1;
-				else
-					;// nothing
-			end
-		2'b10:		
-			begin
-				db_low_en = 1'b1;
-			end
-		2'b11:
-			begin
-				db_high_en = 1'b1;
-			end
-	endcase
-end
+
 
 always_comb begin
 	next_state = IDLE; // default state
 	tx_begin = 1'b0;
-	transmit_buffer_en = 1'b0;
 	
 	case(state)
 	
@@ -169,19 +136,14 @@ always_comb begin
 			begin
 				if (iocs & !iorw)
 					next_state = WRITE;
+				else if (iocs & iorw)
+					next_state = READ;
 				else
 					next_state = IDLE;
 			end
 			
 		READ:  
 			begin
-				
-			
-			
-			end
-			
-		WRITE:
-			begin 
 				
 			
 			
